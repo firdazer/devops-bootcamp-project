d014676c075d337cc15f996d5d31133a1e4b12d8
Firdauz <firdauzraiz@yahoo.com>
2026-09-01 11:46:10 +0800

    Add ec2 Terraform module

---
+++ terraform/ec2/network.tf
@@ -0,0 +1,15 @@
+module "my_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name = "tf-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = ["ap-southeast-1a"]
+
+  public_subnets  = ["10.0.0.0/26"]
+  private_subnets = ["10.0.0.128/26"]
+
+  map_public_ip_on_launch = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+}
