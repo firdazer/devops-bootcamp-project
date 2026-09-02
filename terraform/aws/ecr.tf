ae2366923300f599291adfd8eb1fc5b1b53576aa
Firdauz <firdauzraiz@yahoo.com>
2026-09-02 21:27:36 +0800

    Add multi-stage Dockerfile, ECR repo, geerlingguy.docker role, and GitHub Actions workflow

---
+++ terraform/aws/ecr.tf
@@ -0,0 +1,57 @@
+resource "aws_ecr_repository" "app" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+  force_delete         = false
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = var.ecr_repository_name
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 7 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep last 10 tagged images"
+        selection = {
+          tagStatus   = "tagged"
+          tagPrefixList = ["latest", "v"]
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.app.repository_url
+}
--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -3,3 +3,9 @@ variable "az" {
   type        = string
   default     = "ap-southeast-1a"
 }
+
+variable "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "devops-bootcamp-project/app"
+}
